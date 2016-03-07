f868a7345820b680ed81433aea61d01baac08df0
Christopher Hughes <christopher.hughes@thomsonreuters.com>
2016-03-07 08:57:33 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,8 +1,9 @@
 shinyUI(pageWithSidebar(
   headerPanel("The World in Food"),
   sidebarPanel(
-    sliderInput('slideYear', 'Select the Year',value = 1990, min = 1970, max = 2011, step = 1,)
-  ),
+    sliderInput('slideYear', 'Select the Year',value = 1990, min = 1970, max = 2011, step = 1,),
+    submitButton('Submit')
+    ),
   mainPanel(
     plotOutput('newplot')
   )
